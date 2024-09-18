36f7ef1726218b44ebca6662b99d53dfeded349e
CTPache <33907485+CTPache@users.noreply.github.com>
2024-09-18 21:34:37 +0200

    Agregado Assembly-CSharp-firstpass y dependencias

---
+++ Assets/Plugins/Assembly-CSharp-firstpass/InControl/NativeInputOptions.cs
@@ -0,0 +1,11 @@
+namespace InControl
+{
+	public struct NativeInputOptions
+	{
+		public ushort updateRate;
+
+		public int enableXInput;
+
+		public int preventSleep;
+	}
+}
---
+++ Assets/Plugins/Assembly-CSharp-firstpass/InControl/VersionInfo.cs
@@ -0,0 +1,169 @@
+using System;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace InControl
+{
+	public struct VersionInfo : IComparable<VersionInfo>
+	{
+		public int Major;
+
+		public int Minor;
+
+		public int Patch;
+
+		public int Build;
+
+		public static VersionInfo Min
+		{
+			get
+			{
+				return new VersionInfo(int.MinValue, int.MinValue, int.MinValue, int.MinValue);
+			}
+		}
+
+		public static VersionInfo Max
+		{
+			get
+			{
+				return new VersionInfo(int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue);
+			}
+		}
+
+		public VersionInfo Next
+		{
+			get
+			{
+				return new VersionInfo(Major, Minor, Patch, Build + 1);
+			}
+		}
+
+		public VersionInfo(int major, int minor, int patch, int build)
+		{
+			Major = major;
+			Minor = minor;
+			Patch = patch;
+			Build = build;
+		}
+
+		public static VersionInfo InControlVersion()
+		{
+			VersionInfo result = default(VersionInfo);
+			result.Major = 1;
+			result.Minor = 7;
+			result.Patch = 0;
+			result.Build = 9320;
+			return result;
+		}
+
+		public static VersionInfo UnityVersion()
+		{
+			Match match = Regex.Match(Application.unityVersion, "^(\\d+)\\.(\\d+)\\.(\\d+)");
+			VersionInfo result = default(VersionInfo);
+			result.Major = Convert.ToInt32(match.Groups[1].Value);
+			result.Minor = Convert.ToInt32(match.Groups[2].Value);
+			result.Patch = Convert.ToInt32(match.Groups[3].Value);
+			result.Build = 0;
+			return result;
+		}
+
+		public int CompareTo(VersionInfo other)
+		{
+			if (Major < other.Major)
+			{
+				return -1;
+			}
+			if (Major > other.Major)
+			{
+				return 1;
+			}
+			if (Minor < other.Minor)
+			{
+				return -1;
+			}
+			if (Minor > other.Minor)
+			{
+				return 1;
+			}
+			if (Patch < other.Patch)
+			{
+				return -1;
+			}
+			if (Patch > other.Patch)
+			{
+				return 1;
+			}
+			if (Build < other.Build)
+			{
+				return -1;
+			}
+			if (Build > other.Build)
+			{
+				return 1;
+			}
+			return 0;
+		}
+
+		public static bool operator ==(VersionInfo a, VersionInfo b)
+		{
+			return a.CompareTo(b) == 0;
+		}
+
+		public static bool operator !=(VersionInfo a, VersionInfo b)
+		{
+			return a.CompareTo(b) != 0;
+		}
+
+		public static bool operator <=(VersionInfo a, VersionInfo b)
+		{
+			return a.CompareTo(b) <= 0;
+		}
+
+		public static bool operator >=(VersionInfo a, VersionInfo b)
+		{
+			return a.CompareTo(b) >= 0;
+		}
+
+		public static bool operator <(VersionInfo a, VersionInfo b)
+		{
+			return a.CompareTo(b) < 0;
+		}
+
+		public static bool operator >(VersionInfo a, VersionInfo b)
+		{
+			return a.CompareTo(b) > 0;
+		}
+
+		public override bool Equals(object other)
+		{
+			if (other is VersionInfo)
+			{
+				return this == (VersionInfo)other;
+			}
+			return false;
+		}
+
+		public override int GetHashCode()
+		{
+			return Major.GetHashCode() ^ Minor.GetHashCode() ^ Patch.GetHashCode() ^ Build.GetHashCode();
+		}
+
+		public override string ToString()
+		{
+			if (Build == 0)
+			{
+				return string.Format("{0}.{1}.{2}", Major, Minor, Patch);
+			}
+			return string.Format("{0}.{1}.{2} build {3}", Major, Minor, Patch, Build);
+		}
+
+		public string ToShortString()
+		{
+			if (Build == 0)
+			{
+				return string.Format("{0}.{1}.{2}", Major, Minor, Patch);
+			}
+			return string.Format("{0}.{1}.{2}b{3}", Major, Minor, Patch, Build);
+		}
+	}
+}
